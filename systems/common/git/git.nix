d20218ba6084a0003612646b76b6f5d10c3827cc
Niels de Koeijer <NEMK@bang-olufsen.dk>
2026-01-02 11:13:17 +0100

    git

--- systems/common/git/git.nix
+++ systems/common/git/git.nix
@@ -3,6 +3,6 @@
   programs.git = {
     enable = true;
     settings.user.name = "Niels de Koeijer";
-    settings.user.email = "NEMK@bang-olufsen.com";
+    settings.user.email = "NEMK@bang-olufsen.dk";
   };
 }
