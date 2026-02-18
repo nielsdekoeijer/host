57a2180ce47dac25e07f0d57172de49dddf72149
Niels de Koeijer <nielsdekoeijer@gmail.com>
2026-02-18 15:11:54 +0100

    fix: iso

--- lib/mkISO.nix
+++ lib/mkISO.nix
@@ -13,16 +13,24 @@ in lib.nixosSystem {
     disko.nixosModules.disko
 
     ({ pkgs, ... }: {
-      nixpkgs.config.allowUnfree = true;
-      hardware.enableAllFirmware = true;
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
       networking.networkmanager.enable = true;
       networking.wireless.enable = lib.mkForce false;
 
+      # to get wifi working... I just iterated until it worked
+      nixpkgs.config.allowUnfree = true;
+      hardware.enableAllFirmware = true;
+      boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot.supportedFilesystems.zfs = lib.mkForce false;
+      hardware.enableRedistributableFirmware = lib.mkForce true;
+      hardware.wirelessRegulatoryDatabase = true;
+
+      # ssh
       services.openssh.enable = true;
 
       environment.systemPackages = with pkgs; [
+        linux-firmware
         git
         neovim
         curl
