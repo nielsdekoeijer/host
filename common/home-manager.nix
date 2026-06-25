e28164dd5e4bf467732ab787e3207dd76700a22f
Niels de Koeijer <nielsdekoeijer@gmail.com>
2026-06-25 18:22:51 +0200

    feat: obsidian-pdf-plus

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -4,6 +4,41 @@
   stateVersion,
   ...
 }:
+let
+  # Build the pdf++ plugin manually using exact release assets
+  obsidian-pdf-plus = pkgs.stdenv.mkDerivation rec {
+    pname = "obsidian-pdf-plus";
+    version = "0.40.31";
+
+    # Skip unpacking since we are downloading raw files, not an archive
+    dontUnpack = true;
+
+    installPhase = ''
+      mkdir -p $out
+
+      cp ${
+        pkgs.fetchurl {
+          url = "https://github.com/RyotaUshio/obsidian-pdf-plus/releases/download/${version}/main.js";
+          sha256 = "3fd395ca015ff812df8a48e75da0ec7a7ab5a6f59406d375470898b1f72aac93";
+        }
+      } $out/main.js
+
+      cp ${
+        pkgs.fetchurl {
+          url = "https://github.com/RyotaUshio/obsidian-pdf-plus/releases/download/${version}/manifest.json";
+          sha256 = "dca50165b74316cdb2b76c92084374997be7949d216b31efcecc2b82ff4a086c";
+        }
+      } $out/manifest.json
+
+      cp ${
+        pkgs.fetchurl {
+          url = "https://github.com/RyotaUshio/obsidian-pdf-plus/releases/download/${version}/styles.css";
+          sha256 = "4d944d6ea7aa581fdcd8367c9d8ab09f37c5a8c594e336547795f79f2ede4066";
+        }
+      } $out/styles.css
+    '';
+  };
+in
 {
   home.packages = [
     # formatters
@@ -71,10 +106,11 @@
       ];
 
       communityPlugins = [
+        obsidian-pdf-plus
       ];
 
       hotkeys = {
-          "command-palette:open" = [ { key = "F1"; } ];
+        "command-palette:open" = [ { key = "F1"; } ];
       };
     };
   };
